3c0937dd0b91193b07599460a0f1e3208fdc59e5
Mingze Xiao <mxiao666@gmail.com>
2018-06-30 22:47:31 -0700

    random commit

---
+++ PlayingCard/PlayingCard/PlayingCardView.swift
@@ -0,0 +1,17 @@
+//
+//  PlayingCardView.swift
+//  PlayingCard
+//
+//  Created by XMZ on 06/24/2018.
+//  Copyright © 2018 Jogchat. All rights reserved.
+//
+
+import UIKit
+
+class PlayingCardView: UIView {
+
+    override func draw(_ rect: CGRect) {
+        
+    }
+
+}
