fdaae1fa13ec3c0f226e89b2c0c8284ca25b5cd3
Mingze Xiao <mxiao666@gmail.com>
2018-08-05 20:23:17 -0700

    add material

--- ReferHelper/Pods/Target Support Files/Pods-ReferHelper/Pods-ReferHelper-frameworks.sh
+++ ReferHelper/Pods/Target Support Files/Pods-ReferHelper/Pods-ReferHelper-frameworks.sh
@@ -144,13 +144,15 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Cards/Cards.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Material/Material.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Motion/Motion.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Player/Player.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/ViewAnimator/ViewAnimator.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Cards/Cards.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Material/Material.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Motion/Motion.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Player/Player.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/ViewAnimator/ViewAnimator.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
